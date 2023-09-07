117b6a43e3a6213777643d934e69e18b6d22cc0d
John Dale <unitejohndalev@gmail.com>
2023-09-07 16:08:06 +0800

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+
+import { HashRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
   <React.StrictMode>
-  <Router>
       <App />
-  </Router>
     </React.StrictMode>
+  </HashRouter>
 );
